01d3a470688bdbe7ecd5e9ff7aab4c1ecb321c16
ChristopherRiveraHernandez <149515006+Chris-k3@users.noreply.github.com>
2026-09-22 19:03:44 -0600

    feat: cascaron inicial Spring Boot con conexion y estructura de paquetes

---
+++ src/main/java/com/example/zentia/zentia/ZentiaApplication.java
@@ -0,0 +1,13 @@
+package com.example.zentia.zentia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZentiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZentiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/zentia/zentia/ZentiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zentia.zentia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZentiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
